9a8da9ea683cd299429e40c1ae329aa8198de123
Xiang CHEN <xiangchen.cs@gmail.com>
2014-07-25 21:23:33 +0800

    small changes

--- backpack.py
+++ backpack.py
@@ -1,16 +1,16 @@
 # parameters
-weight=[2,1,3,2,1]
-value=[12,10,20,15,8]
-capacity=5
+weight = [2, 1 ,3, 2, 1]
+value = [12, 10, 20, 15, 8]
+capacity = 5
 # dynamic programming
-num=len(weight)
-opt=[0]*(capacity+1)
-optori=[0]*(capacity+1)
+num = len(weight)
+opt = [0]*(capacity+1)
+optori = [0]*(capacity+1)
 for i in range(0,num):
 	for j in range(0,capacity+1):
-		optori[j]=opt[j]
+		optori[j] = opt[j]
 	for j in range(0,capacity+1-weight[i]):
-		if optori[j+weight[i]]<optori[j]+value[i]:
-			opt[j+weight[i]]=optori[j]+value[i]
+		if optori[j+weight[i]] < optori[j]+value[i]:
+			opt[j+weight[i]] = optori[j]+value[i]
 		print opt
 print opt[capacity]
---
+++ readme.md
@@ -0,0 +1,3 @@
+This repository is used for realizing some basic structures and algorithms.
+Matlab, python, java, c++ may be practiced.
+And, most importantly, **have fun.**
